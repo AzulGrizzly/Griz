4059cb81513c74803c03120125dabcf0258ad65e
AzulGrizzly <92818169+AzulGrizzly@users.noreply.github.com>
2022-04-18 14:54:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ openFile = tk.Button(root, text='Open File', padx=10, pady=5, fg='white', bg="#2
 
 openFile.pack()
 
-runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg='#263D42', command='runApps')
+runApps = tk.Button(root, text='Run Apps', padx=10, pady=5, fg='white', bg='#263D42', command=runApps)
 runApps.pack()
 
 
